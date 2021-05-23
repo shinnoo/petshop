06a0b3ed09caab9027ac2724f3652d1e0bf521b7
trandung.ptit <trandung164.ptit@gmail.com>
2021-05-23 21:55:04 +0700

    fix zuul

--- zuul-server/src/main/java/io/github/tubean/eureka/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/io/github/tubean/eureka/zuulserver/ZuulServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 @EnableEurekaClient        // It acts as a eureka client
@@ -12,6 +14,11 @@ public class ZuulServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
+    @Bean
+    ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 
 }
