2008eb35beea11b7832612796ea4ad9f6c2866a5
trandung.ptit <trandung164.ptit@gmail.com>
2021-05-23 22:54:37 +0700

    a

--- zuul-server/src/main/java/io/github/tubean/eureka/zuulserver/config/RedirectionFilter.java
+++ zuul-server/src/main/java/io/github/tubean/eureka/zuulserver/config/RedirectionFilter.java
@@ -2,13 +2,14 @@ package io.github.tubean.eureka.zuulserver.config;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.ProxyRequestHelper;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
 
-@Component
 public class RedirectionFilter extends ZuulFilter {
-
 	@Override
 	public String filterType() {
 		return "pre";
@@ -16,7 +17,7 @@ public class RedirectionFilter extends ZuulFilter {
 
 	@Override
 	public int filterOrder() {
-		return 2;
+		return 1;
 	}
 
 	@Override
@@ -26,22 +27,8 @@ public class RedirectionFilter extends ZuulFilter {
 
 	@Override
 	public Object run() {
-		RequestContext ctx = RequestContext.getCurrentContext();
-		HttpServletRequest request = ctx.getRequest();
-		ctx.addZuulRequestHeader("Content-Type", "application/json");
-//		String header = request.getHeader("YOUR_HEADER_PARAM");
-//
-//		if ("YOUR_A_LOGIC".equals(header) ) {
-//			ctx.put("serviceId", "serviceA");
-//			//ctx.setRouteHost(new URL("http://Service_A_URL”));
-//		} else { // "YOUR_B_LOGIC"
-//			ctx.put("serviceId", "serviceB");
-//			//ctx.setRouteHost(new URL("http://Service_B_URL”));
-//		}
-//		log.info(String.format("%s requ
-//		est to %s", request.getMethod(),
-//				request.getRequestURL().toString()));
+		RequestContext context = RequestContext.getCurrentContext();
+		context.addZuulRequestHeader("Content-Type", "application/json");
 		return null;
 	}
 }
-
